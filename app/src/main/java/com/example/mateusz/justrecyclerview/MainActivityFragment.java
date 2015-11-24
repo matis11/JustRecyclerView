afcbf28af31b485bd775f38aeb1a1b94c8d90257
Michal Moczulski <moczul@gmail.com>
2015-11-24 21:24:03 +0100

    Make RecyclerView works

--- app/src/main/java/com/example/mateusz/justrecyclerview/MainActivityFragment.java
+++ app/src/main/java/com/example/mateusz/justrecyclerview/MainActivityFragment.java
@@ -1,23 +1,14 @@
 package com.example.mateusz.justrecyclerview;
 
-import android.os.Bundle;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
 
 public class MainActivityFragment extends BaseRecyclerFragment<HomeThreadListAdapter> {
 
     public static final String TAG = "Basic Tag";
 
     public MainActivityFragment() {
-    }
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main, container, false);
     }
 
     @Override
--- app/src/main/java/com/example/mateusz/justrecyclerview/MyItem.java
+++ app/src/main/java/com/example/mateusz/justrecyclerview/MyItem.java
@@ -3,8 +3,14 @@ package com.example.mateusz.justrecyclerview;
 import java.io.Serializable;
 
 public class MyItem implements Serializable {
+
     private String title;
     private String content;
+
+    public MyItem(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
 
     public String getTitle() {
         return title;
